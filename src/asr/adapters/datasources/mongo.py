08e23c96a1df02f9a87cd3b163c3b8439dbd8821
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-20 16:39:05 +0530

    added log

--- src/asr/adapters/datasources/mongo.py
+++ src/asr/adapters/datasources/mongo.py
@@ -9,6 +9,7 @@ class MongoClient:
     @classmethod
     def get_connection(cls, db_name: str = Config.MONGO_DB_NAME):
         if cls.__connection is None:
+            print(Config.MONGO_URL)
             cls.__connection = pymongo.MongoClient(Config.MONGO_URL)
 
         return cls.__connection[db_name]
