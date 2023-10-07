3f9245ac8935ed6b2e2d684adc6dce5d27b8c00f
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-10-07 10:35:14 +0530

    updated scheduler

--- src/asr/scheduler.py
+++ src/asr/scheduler.py
@@ -22,10 +22,12 @@ load_dotenv('schedule.env')
 logger = getLogger(__name__)
 
 
-cdr_collection = MongoClient(
-    environ.get('RECORDINGS_DB_URL')
-)[environ.get('RECORDINGS_DB_NAME')].get_collection('cdr')
-asr_feed_db = MongoClient()['myFirstDatabase']
+db_connection = MongoClient(
+    environ.get('MONGO_DB_URL')
+)[environ.get('DB_NAME')]
+
+
+cdr_collection = db_connection.get_collection('cdr')
 pipeline = [
     {
         '$project': {
@@ -54,15 +56,15 @@ pipeline = [
 
 
 def _extract_recordings():
-    one_hour_ago = datetime.now() - timedelta(hours=1)
-    pipeline[1]['$match']['time']['$gte'] = one_hour_ago
+    past_time = datetime.now() - timedelta(minutes=45)
+    pipeline[1]['$match']['time']['$gte'] = past_time
 
     return cdr_collection.aggregate(pipeline)
 
 
 def extract_and_load():
     logger.info('Starting Extraction')
-    repo = ASRFeedRepository(asr_feed_db)
+    repo = ASRFeedRepository(db_connection)
     asr_feeds = ASRFeedService(
         repo,
         ExternalAPIClient(),
