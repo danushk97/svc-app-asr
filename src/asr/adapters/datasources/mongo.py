bd1a5b426fcd53ef94ce2f197e9c08d27c7fdcb0
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-11-02 10:39:32 +0530

    updated mongo connection

--- src/asr/adapters/datasources/mongo.py
+++ src/asr/adapters/datasources/mongo.py
@@ -9,6 +9,7 @@ class MongoClient:
     @classmethod
     def get_connection(cls):
         if cls.__connection is None:
-            cls.__connection = pymongo.MongoClient(Config.MONGO_URL)
+            cls.__connection = pymongo.MongoClient(Config.MONGO_URL)\
+                .get_database(Config.MONGO_DB_NAME)
 
-        return cls.__connection[Config.MONGO_DB_NAME]
+        return cls.__connection
